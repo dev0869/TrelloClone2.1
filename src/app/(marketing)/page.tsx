2b1809d7dd3bc53b08eac3afba19244facb87b4a
dev0869 <deveshbisht36@gmail.com>
2023-12-13 15:01:23 +0530

    Home page implemented

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Link from "next/link";
+const Home = () => {
+  return (
+    <div className="flex justify-center h-screen">
+      <div className="flex items-center justify-center flex-col">
+        <div className={"flex items-center justify-center flex-col"}>
+          <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
+            No 1 task managment
+          </div>
+          <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+            Taskify helps team move
+          </h1>
+          <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+            work forward.
+          </div>
+        </div>
+        <div
+          className={
+            "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto"
+          }
+        >
+          Collaborate, manage projects, and reach new productivity peaks. From
+          high rises to the home office, the way your team works is unique -
+          accomplish it all with Taskify.
+        </div>
+        <button className="mt-6">
+          <Link href="/sign-up">Get Taskify for free</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
